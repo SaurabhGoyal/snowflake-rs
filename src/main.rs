ba876aa4f624f21e0ac70167e1d7e7d91ffcd218
Saurabh Goyal <saurabh.2561@gmail.com>
2024-05-05 12:47:56 +0530

    add error handling, add doc comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::time::SystemTime;
+use std::{
+    thread,
+    time::{Duration, SystemTime},
+};
 
 const UID_BIT_COUNT: u64 = 63;
 const MILLISECONDS_IN_YEAR: u64 = 1_000 * 86_400 * 365;
 
 const DEFAULT_TIMESTAMP_BIT_COUNT: u64 = 42;
 const DEFAULT_NODE_ID_BIT_COUNT: u64 = 11;
+const MIN_SEQUENCE_ID_BIT_COUNT: u64 = 4;
 
 fn main() {
     let cfg = Config::default();
@@ -18,8 +22,7 @@ fn main() {
     }
 }
 
-// UID Generator
-
+/// Generator provides an instance that can generate unique ids for a given node_id.
 struct Generator {
     config: Config,
     node_id: u64,
@@ -29,6 +32,10 @@ struct Generator {
 
 impl Generator {
     fn from(config: Config, node_id: u64) -> Self {
+        let max_node_id = (1 << config.node_id_bit_count) - 1;
+        if node_id > max_node_id {
+            panic!("Node id out of range - [0, {max_node_id}].")
+        }
         Self {
             config,
             node_id,
@@ -37,10 +44,18 @@ impl Generator {
         }
     }
 
+    /// Get returns a unique id which is generated in a sequence maintained per millisecond window.
+    /// If the load is higher than the number of unique ids that can be generated for a specific
+    /// millisecond window, then the methods sleeps till next millisecond window to ensure uniqueness.
     fn get(&mut self) -> u64 {
         let curr_ts = self.ms_since_epoch() as u64;
         if curr_ts <= self.last_ts {
             self.last_seq_id += 1;
+            if self.last_seq_id == (1 << self.config.node_id_shift() - 1) {
+                thread::sleep(Duration::from_millis(1));
+                self.last_ts = curr_ts;
+                self.last_seq_id = 0;
+            }
         } else {
             self.last_ts = curr_ts;
             self.last_seq_id = 0;
@@ -60,8 +75,9 @@ impl Generator {
 
 // End UID Generator
 
-// Config
-
+/// Config controls the UID generation, mainly controlling the number of nodes and number of
+/// unique ids per millisecond that a node can generate. This is tradeoff between how fast
+/// you need to generate unique ids and across how many servers.
 struct Config {
     timestamp_bit_count: u64,
     node_id_bit_count: u64,
@@ -72,7 +88,15 @@ impl Config {
         Self::from(DEFAULT_TIMESTAMP_BIT_COUNT, DEFAULT_NODE_ID_BIT_COUNT)
     }
 
+    /// Config::from generates a config to be used by generator to generate IDs.
+    ///  - Use higher or lower timestamp_bit_count based on how long (generally in years) the lifecycle of a generated unique id should be.
+    ///  - Use higher or lower node_id_bit_count based on how many servers are going to be involved in unique id generation.
+    ///  - Above two values directly impact the range of unique ids that one server can generate per millisecond while being within the constraint of 64 bit.
+    /// Choose above wisely as higher range per server gives better performance in high throughput systems.
     fn from(timestamp_bit_count: u64, node_id_bit_count: u64) -> Self {
+        if timestamp_bit_count + node_id_bit_count + MIN_SEQUENCE_ID_BIT_COUNT > UID_BIT_COUNT {
+            panic!("Unable to accomodate the given config in {UID_BIT_COUNT} bit id.")
+        }
         Self {
             timestamp_bit_count,
             node_id_bit_count,
